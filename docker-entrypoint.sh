4887ffb4c5d34be4795b5fd7c7f41846dfb573ac
william1nguyen <vietanhhd03@gmail.com>
2025-03-05 02:43:29 +0700

    fix(server): update settings and README

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 python manage.py makemigrations
 python manage.py migrate
-gunicorn ecommerce.wsgi:application -b 0.0.0.0:8080 -w 3 -k gevent --name ecoin --timeout 200 --keep-alive 30
+gunicorn ecommerce.wsgi:application -b 0.0.0.0:8080 -w 3 -k gevent --name xshop --timeout 200 --keep-alive 30
